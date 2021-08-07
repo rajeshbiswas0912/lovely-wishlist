1b68ccb7a66fd667ab2f9344994fcb4aead11a44
Rajesh Biswas <rajesh.weavers@gmail.com>
2021-08-07 17:58:25 +0530

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,14 @@
-working...
+@extends('shopify-app::layouts.default')
+
+@section('content')
+    <!-- You are: (shop domain name) -->
+    <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
+@endsection
+
+@section('scripts')
+    @parent
+
+    <script>
+        actions.TitleBar.create(app, { title: 'Welcome' });
+    </script>
+@endsection
